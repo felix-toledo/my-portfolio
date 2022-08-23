6472a5fe602d85412cdeebbf8c657aefc9975a7f
Felix Toledo <felixtoledoctes@gmail.com>
2022-08-23 18:53:18 -0300

    first commit

---
+++ app/script.js
@@ -0,0 +1,9 @@
+quienSoyBtn = document.getElementById("quienSoyBtn");
+
+quienSoyBtn.addEventListener("click", ()=>{navbar("quiensoy")})
+
+function navbar(section){
+    if (section == "quiensoy"){
+        alert("asd")
+    }
+}
